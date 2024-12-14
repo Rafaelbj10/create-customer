536626500cd8f3820b2f8a537227dc6d0b4ec1e7
rafaelbatista <rbatistaa321@gmail.com>
2024-12-13 21:52:38 -0300

    primeiro commit

---
+++ src/main/java/com/cdps/CreateCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.cdps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateCustomerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cdps/CreateCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateCustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
